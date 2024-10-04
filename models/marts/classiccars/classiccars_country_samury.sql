7f105d4f33cb564bd3f783b7d1ea3fdad7196e9c
hitoshi matsuda <programmer060732@gmail.com>
2024-10-04 07:18:17 +0000

    【実装】エラー修正

--- models/marts/classiccars/classiccars_country_samury.sql
+++ models/marts/classiccars/classiccars_country_samury.sql
@@ -1,15 +1,18 @@
-with trans_lang as (
-    {{ config(schema='_classiccarsL2') }}
-    select * from {{ ref('trans_jp') }}
-),
+with
+    trans_lang as (
+        {{ config(schema="_classiccarsL2") }} select * from {{ ref("trans_jp") }}
+    ),
 
-source_mart as (
-    {{ config(schema='_classiccars_L2') }}
-    select * from {{ ref('classiccars_customers_payments_employees_offices') }}
-),
+    source_mart as (
+        {{ config(schema="_classiccars_L2") }}
+        select *
+        from {{ ref("classiccars_customers_payments_employees_offices") }}
+    ),
 
-join_table as (
-SELECT t1.officecode, t1.amount, t1.country, t2.jp_name from source_mart t1 left join trans_lang t2 on t1.country = t2.eng_name
-)
+    join_table as (
+        select t1.officecode, t1.amount, t1.country, t2.jp_name
+        from source_mart t1
+        left join trans_lang t2 on t1.country = t2.eng_name
+    )
 
 selecct * from join_table
